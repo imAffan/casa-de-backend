07dece8fb968c8f7b60c719257a23acc601e3669
Affan Ahmed <105587806+imAffan@users.noreply.github.com>
2023-03-07 23:27:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -48,6 +48,7 @@ if (process.env.NODE_ENV === 'production') {
       'http://localhost:3001',
       'http://127.0.0.1:3000',
       'http://localhost:3000',
+      'https://casa-de-vacaciones.netlify.app',
     ],
     credentials: true,
   };
